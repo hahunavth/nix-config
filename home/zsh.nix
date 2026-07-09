578ecc7e8a4a2e7e51ec02d2c22aa3c86c67e0ed
hahunavth <vuthanhha@kodnet.co.jp>
2026-07-09 15:51:38 +0900

    Add port forwarding helpers for ports 5006 and 7129

--- home/zsh.nix
+++ home/zsh.nix
@@ -9,7 +9,9 @@ let
   # kill any existing socat listener on a given local port
   killPort = port: "pkill -f 'TCP-LISTEN:${port},'";
   kill5005 = killPort "5005";
+  kill5006 = killPort "5006";
   kill6090 = killPort "6090";
+  kill7129 = killPort "7129";
   kill7130 = killPort "7130";
   kill9220 = killPort "9220";
 in
@@ -39,6 +41,7 @@ in
       win-5005 = "${kill5005}; ${fwd "5005" "5005"}";
       win-5006 = "${kill5005}; ${fwd "5005" "5006"}";
       win-6090 = "${kill6090}; ${fwd "6090" "6090"}";
+      win-7129 = "${kill7129}; ${fwd "7129" "7129"}";
       win-7130 = "${kill7130}; ${fwd "7130" "7130"}";
       win-9220 = "${kill9220}; ${fwd "9220" "9220"}";
       win-off = "pkill -f 'TCP:${winHost}:'";
